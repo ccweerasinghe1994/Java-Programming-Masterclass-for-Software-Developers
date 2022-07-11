f59ab1d06dd2ea33c43b76ab468ed89a1931d90e
wchamara <chamara.weerasinghe@my.sliit.lk>
2022-07-11 09:16:04 +0530

    adding char

--- CharAndBoolean/src/Main.java
+++ CharAndBoolean/src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        char myChar = 'D';
+        char myUnicodeChar = '\u0044';
+        System.out.println(myChar);
+        System.out.println(myUnicodeChar);
+
     }
 }
