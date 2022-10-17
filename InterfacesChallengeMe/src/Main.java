aff3a940e7d7dfac7a0636f5663e7001028f9ea4
ccweerasinghe1994 <chamara.weerasinghe@my.sliit.lk>
2022-10-17 06:33:06 +0530

    📜🆕:  Interfaces Challenge Part 2. closee #55

--- InterfacesChallengeMe/src/Main.java
+++ InterfacesChallengeMe/src/Main.java
@@ -1,45 +1,49 @@
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-    Player tim = new Player("chamara",12,100);
-        System.out.println(tim.toString());
+        Player tim = new Player("chamara", 12, 100);
+        System.out.println(tim);
 
         tim.setHitPoints(122);
-        System.out.println(tim.toString());
+        System.out.println(tim);
 
         tim.setWeapon("Storm bringer");
-        System.out.println(tim.toString());
+        System.out.println(tim);
         System.out.println("---------------------------------------------------------------");
         saveObject(tim);
         loadObject(tim);
-        System.out.println(tim.toString());
+        System.out.println(tim);
 
+        System.out.println("----------- Monster section----------------------------");
 
+        ISavable wearwolf = new Monster("wearwolf", 12, 12);
+//         here we can cast the object to the Monster or use the monster instead;
+        System.out.println(((Monster) wearwolf).getName());
+        saveObject(wearwolf);
 
     }
 
-    public static ArrayList<String> readvalues(){
+    public static ArrayList<String> readvalues() {
         Scanner scanner = new Scanner(System.in);
         ArrayList<String> values = new ArrayList<>();
         boolean quit = false;
         int choice;
         printInstruction();
         int index = 0;
-        while (!quit){
+        while (!quit) {
             System.out.println("please enter choice");
             choice = scanner.nextInt();
             scanner.nextLine();
-            switch (choice){
-                case 0 ->{
+            switch (choice) {
+                case 0 -> {
                     quit = true;
                 }
-                case 1->{
+                case 1 -> {
                     System.out.println("please enter a name");
                     String value = scanner.nextLine();
-                    values.add(index,value);
+                    values.add(index, value);
                     index++;
                 }
             }
@@ -47,21 +51,21 @@ public class Main {
         return values;
     }
 
-    public static void printInstruction(){
+    public static void printInstruction() {
         System.out.println("""
                 press
                 0 - to exit
-                1 - to enter a value
+                1 - to enter a choice
                 """);
     }
 
-    public static void saveObject(ISavable objectToSave){
-        for(String item:objectToSave.write()){
-            System.out.println("saving "+item+" to the storage device");
+    public static void saveObject(ISavable objectToSave) {
+        for (String item : objectToSave.write()) {
+            System.out.println("saving " + item + " to the storage device");
         }
     }
 
-    public static void loadObject(ISavable objectTLoad){
+    public static void loadObject(ISavable objectTLoad) {
         ArrayList<String> values = readvalues();
         objectTLoad.read(values);
     }
