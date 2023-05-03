11221649e4c19ce646ac016391e21a506165b3fd
ccweerasinghe1994 <chamara.weerasinghe@my.sliit.lk>
2023-05-03 22:07:51 +0530

    Update Main.java

--- ControlFlow_14/src/Main.java
+++ ControlFlow_14/src/Main.java
@@ -8,6 +8,23 @@ public class Main {
             case 3, 4, 5-> System.out.println("Value is 3, 4 or 5");
             default-> System.out.println("Value is neither 1 nor 2,3,4,5");
         }
+        System.out.println(getQuarter("January"));
+        System.out.println(getQuarter("April"));
+        System.out.println(getQuarter("July"));
+        System.out.println(getQuarter("October"));
+        System.out.println(getQuarter("December"));
+        System.out.println(getQuarter("Unknown"));
 
+    }
+
+    public static String getQuarter(String month){
+        String quarter = switch (month) {
+            case "January", "February", "March" -> "First Quarter";
+            case "April", "May", "June" -> "Second Quarter";
+            case "July", "August", "September" -> "Third Quarter";
+            case "October", "November", "December" -> "Fourth Quarter";
+            default -> "Unknown";
+        };
+        return quarter;
     }
 }
