50df6a41361ff4f78d0d57048755ec1556372034
ccweerasinghe1994 <chamara.weerasinghe@my.sliit.lk>
2023-05-03 13:30:43 +0530

    expressions

---
+++ KeyWordAndExpressions_12/src/KeyWordAndExpressions.java
@@ -0,0 +1,6 @@
+public class KeyWordAndExpressions {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ KeyWordAndExpressions_12/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args) {
+//        this is a valid statement
+//        double is not part of the expression
+        double kilometers = (100 * 1.609344);
+
+        int highScore = 50;
+        if (highScore >= 25) {
+            highScore += 1000;
+        }
+    }
+}
