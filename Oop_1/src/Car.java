97d5fd35f8983964f40c05817c982161c612e99b
ccweerasinghe1994 <chamara.weerasinghe@my.sliit.lk>
2023-05-04 00:07:46 +0530

    adding getter and setters

--- Oop_1/src/Car.java
+++ Oop_1/src/Car.java
@@ -1,14 +1,60 @@
 public class Car {
-    private String make;
-    private String model;
-    private String color;
+    private String make = "Porsche";
+    private String model = "Carrera";
+    private String color = "Red";
 
-    private int doors;
+    private int doors = 2;
 
-    private boolean convertible;
+    private boolean convertible = false;
 
-    private void describe(){
+    public String getMake() {
+        return make;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getDoors() {
+        return doors;
+    }
+
+    public void setDoors(int doors) {
+        this.doors = doors;
+    }
+
+    public boolean isConvertible() {
+        return convertible;
+    }
+
+    public void setConvertible(boolean convertible) {
+        this.convertible = convertible;
+    }
+
+    public void describe() {
         System.out.println("This is a car");
-        System.out.println("this car make is " + this.make + " and model is " + this.model+ " and color is " + this.color);
+        System.out.println("this car make is " +
+                this.make + " and model is " +
+                this.model + " and color is "
+                + this.color
+                + " and doors are " + this.doors
+                + " and convertible is " + this.convertible
+        );
     }
 }
--- Oop_1/src/Main.java
+++ Oop_1/src/Main.java
@@ -1,9 +1,12 @@
 public class Main {
     public static void main(String[] args) {
 
-//        new car
         Car porsche = new Car();
-
-        Car holden = new Car();
+        porsche.describe();
+        System.out.println(porsche.getMake());
+        System.out.println(porsche.getModel());
+        System.out.println(porsche.getColor());
+        System.out.println(porsche.getDoors());
+        System.out.println(porsche.isConvertible());
     }
 }
